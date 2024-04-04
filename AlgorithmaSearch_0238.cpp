10af38cff3cb2ac1d68a359d9f736f9147e1dc6c
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2024-04-04 14:57:42 +0700

    menambahkan decision making if..else untuk menentukan apakah program akan melanjutkan eksekusi atau menampilkan pesan kesalahan

--- AlgorithmaSearch_0238.cpp
+++ AlgorithmaSearch_0238.cpp
@@ -14,5 +14,9 @@ void input()
 	{
 		cout << "Enter the number of elements in the array : ";
 		cin >> n;
+		if ((n > 0) && (n <= 20))
+			break;
+		else
+			cout << "\nArray shoould have minimum 1 and maximum 20 elements.\n\n";
 	}
 }
